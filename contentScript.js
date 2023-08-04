83234a2a8dfea3cb0f6e90e2122e32afeefa0a82
Nishiv Singh <singh.136@iitj.ac.in>
2023-08-04 22:02:59 +0530

    Added the functionality to store timestamps in chrome storage

--- contentScript.js
+++ contentScript.js
@@ -1,6 +1,7 @@
 (() => {
   let ytRightControls, ytPlayer;
   let currentVideo = new String();
+  let currentVideoBookmarks = [];
   chrome.runtime.onMessage.addListener((obj, sender, response) => {
     const { type, value, videoId } = obj;
 
@@ -10,15 +11,24 @@
     }
   });
 
-  const newVideoLoaded = () => {
+  const fetchBookmarks = () => {
+    return new Promise((resolve) => {
+      chrome.storage.sync.get([currentVideo], (obj) => {
+        resolve(obj[currentVideo] ? JSON.parse(obj[currentVideo]) : []);
+      });
+    });
+  };
+  const newVideoLoaded = async () => {
     const bookmarkBtnExists =
       document.getElementsByClassName("bookmark-btn")[0];
+    currentVideoBookmarks = await fetchBookmarks();
 
     if (!bookmarkBtnExists) {
       const bookmarkBtn = document.createElement("img");
 
       bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
       bookmarkBtn.className = "ytp-button " + "bookmark-btn";
+      bookmarkBtn.style = "scale: 0.5;";
       bookmarkBtn.title = "Click to bookmark current timestamp";
 
       ytRightControls =
@@ -26,7 +36,29 @@
       ytPlayer = document.getElementsByClassName("video-stream")[0];
 
       ytRightControls.prepend(bookmarkBtn);
+      bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
     }
+  };
+  const addNewBookmarkEventHandler = async () => {
+    const currentTime = ytPlayer.currentTime;
+
+    const newBookmark = {
+      time: currentTime,
+      desc: "Bookmark at " + convert(currentTime),
+    };
+
+    currentVideoBookmarks = await fetchBookmarks();
+    chrome.storage.sync.set({
+      [currentVideo]: JSON.stringify(
+        [...currentVideoBookmarks, newBookmark].sort((a, b) => a.time - b.time)
+      ),
+    });
   };
   newVideoLoaded();
 })();
+
+const convert = (t) => {
+  var date = new Date(0);
+  date.setSeconds(t);
+  return date.toISOString().substring(11, 19);
+};
